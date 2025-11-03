01fceb24f8b144065f954a9d822ca68be4edf237
Leon Khidilov <khidilov@gmail.com>
2025-11-03 19:12:22 +0400

    Learned about lifting states up so that sibling element could use them as well

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats />
+    </div>
+  );
+}
+
+const Logo = () => <h1>🏝️ Far away 🧳</h1>;
+
+const Form = ({ onAddItems }) => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
+  };
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((item) => (
+          <option value={item} key={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Items"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+const PackingList = ({ items }) => (
+  <div className="list">
+    <ul>
+      {items.map((i) => (
+        <Item item={i} key={i.id} />
+      ))}
+    </ul>
+  </div>
+);
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+const Stats = () => (
+  <footer className="stats">
+    <em>🧳 You have X items on your list, and you already packed X%</em>
+  </footer>
+);
